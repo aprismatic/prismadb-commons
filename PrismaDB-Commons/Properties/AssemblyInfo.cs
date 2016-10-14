580fdf08cb9492f9cc04019ead63ea33e2fe73ef
bazzilic <bazzilic@gmail.com>
2016-10-14 11:23:52 +0800

    References, assembly info, and .nuspec tweaks

--- PrismaDB-Commons/Properties/AssemblyInfo.cs
+++ PrismaDB-Commons/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PrismaDB-Commons")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Commons for the PrismaDB project.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PrismaDB-Commons")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Vasily Sidorov (bazzilic) / Nanyang Technological University")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
