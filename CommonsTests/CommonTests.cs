277368c3fa0c5236e9e546ef4050fa9226050b74
bazzilic <bazzilic@gmail.com>
2017-11-15 14:15:27 +0800

    cleanup, updates

--- CommonsTests/CommonTests.cs
+++ CommonsTests/CommonTests.cs
@@ -1,24 +1,24 @@
-using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Xunit;
 
 namespace CommonsTests
 {
     public class CommonsTests
     {
-        [Fact]
+        [Fact(DisplayName = "Unique string generation")]
         public void TestGetUniqueString()
         {
-            const int iterations = 1000;
+            const int iterations = 100000;
             var hs = new HashSet<string>(iterations);
 
             for (var i = 0; i < iterations; i++)
             {
-                Assert.Equal(true, hs.Add(PrismaDB.Commons.Helper.GetUniqueString()));
+                Assert.True(hs.Add(PrismaDB.Commons.Helper.GetUniqueString()));
             }
         }
 
-        [Fact]
+        [Fact(DisplayName = "Byte array -> Hex")]
         public void TestByteArrayToHex()
         {
             byte[] data = { 1, 2, 4, 8, 16, 32 };
